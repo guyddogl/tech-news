eb4eef22c46c5f730788968fe3e5e3e464468736
Guyddo <guyddogl@gmail.com>
2023-04-13 15:20:07 -0300

    Requirement 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,7 +32,33 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = (
+        selector.css("div.entry-header-inner h1.entry-title::text")
+        .get()
+        .strip()
+    )
+
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    reading_time = selector.css("li.meta-reading-time::text").get().split()[0]
+    time = "".join(filter(str.isdigit, reading_time))
+    summary = selector.css(
+        "div.entry-content > p:first-of-type *::text"
+    ).getall()
+    summary = "".join(summary).strip()
+    category = selector.css("div.meta-category a span.label::text").get()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(time),
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
