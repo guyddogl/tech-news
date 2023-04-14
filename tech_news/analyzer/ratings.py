79dba803c21fd43eed34b9753049773b27a4a4e4
Guyddo <guyddogl@gmail.com>
2023-04-14 08:43:57 -0300

    Requirement 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,14 @@
+from collections import Counter
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = []
+
+    for news in find_news():
+        categories.append(news["category"])
+
+    count = Counter(sorted(categories))
+
+    return sorted(count, key=count.get, reverse=True)[0:5]
